071c537d9b0c1dc94ff17cd94d153d7bdde10e4e
cme-timothy <timothy.magee@cmeducations.se>
2022-10-24 22:41:54 +0200

    header fixed to bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Main from "./components/Main";
 import Header from "./components/Header";
+import { DataProvider } from "./contexts/DataContext";
 
 function App() {
   return (
     <HelmetProvider>
       <Router>
-        <Flex direction="column" minH="100vh" >
+        <DataProvider>
           <Main />
-          <Flex flex={1} />
+        </DataProvider>
+        <Box position="fixed" bottom="0" width="100%">
           <Header />
-        </Flex>
+        </Box>
       </Router>
     </HelmetProvider>
   );
